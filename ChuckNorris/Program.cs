b5f9aebd8fb54ea6fae6b448a268eb2e5cb526c6
Hele-Mai Ristmäe <hristmae@tktk.ee>
2022-03-23 21:57:48 +0200

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomJoke;
+            randomJoke = GetRandomJokeFromFile("chuck.txt");
+
+            Console.WriteLine($"Here's a joke for you: {randomJoke}.");
+
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string GetRandomJokeFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\OneDrive\Töölaud\Programmeerimise algkursus\Samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+
+    }
+}
